0e849082f576991e9f34113387bfd1b9dbdf82d0
dmitrynikitiuk <dmitrynikitiuk@gmail.com>
2022-06-15 21:15:38 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-long ticket = 14000;
-int bonus = 20;
-long miles = ticket / bonus;
+        long ticket = 14000;
+        int bonus = 20;
+        long miles = ticket / bonus;
         System.out.println("Накоплено бонусных миль:" + miles);
     }
 }
